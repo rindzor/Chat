fcff9b5059d5c0cd8dfcfb06dfb34ba7965b6937
Vladimir Drozdin <rindzorcoop@gmail.com>
2020-01-29 16:51:30 +0200

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by  mac on 1/29/20.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
